8ec1939aab3336d4e605d9b7e0672ee0e326f28f
jasonmziegler <jason@insightfulautomation.com>
2018-12-02 20:29:23 -0600

    add basic bug template

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/',(req, res) => {
-    res.send('Hello, World!');
+    res.send('<h1>Hello, World!</h1>');
 });
 
-app.listen(3000);
+app.get('/hello',(req, res) => {
+    res.render('index');
+});
+
+app.listen(3000, () => {
+    console.log('The app is running on localhost:3000!')
+});
