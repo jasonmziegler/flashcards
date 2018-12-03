1ec5fbf1299f79b1a6910110c80b8c0c71a06df1
jasonmziegler <jason@insightfulautomation.com>
2018-12-02 22:26:12 -0600

    add hello student hello form

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ const app = express();
 app.set('view engine', 'pug');
 
 app.get('/',(req, res) => {
-    res.send('<h1>Hello, World!</h1>');
+    res.render('index');
+});
+
+app.get('/cards',(req, res) => {
+    res.render('card', {prompt:"Who is buried in Grants Tomb", hint:"Think about whose tomb it is!"});
 });
 
 app.get('/hello',(req, res) => {
-    res.render('index');
+    res.render("hello");
+});
+app.post('/hello',(req, res) => {
+    console.dir(req.body);
+    res.render("hello");
+    
 });
 
 app.listen(3000, () => {
